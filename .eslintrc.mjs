6bb2147d0bb65ef31e66b542637df61687527661
tkx.cool <tkx.cool@gmail.com>
2024-07-20 23:13:40 +0800

    feat: first commit

---
+++ .eslintrc.mjs
@@ -0,0 +1,74 @@
+module.exports = {
+  // 指定 ESLint 的解析器和解析器选项
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  // 定义代码的环境。指示哪些全局变量是预定义的
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  // 指示 ESLint 应停止在父目录中查找配置文件
+  root: true,
+  // 扩展其他文件或插件的配置
+  // 扩展了 Next.js ESLint 推荐规则 Prettier TypeScript React 和 React Hooks 的配置
+  extends: [
+    'next',
+    'eslint:recommended',
+    'prettier',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  // ESLint 插件
+  plugins: ['prettier', '@typescript-eslint', 'react', 'react-hooks'],
+  // 指定ESLint规则及其配置
+  rules: {
+    // JavaScript rules
+    'prefer-const': 'warn',
+    'no-var': 'warn',
+    'no-unused-vars': 'warn',
+    'object-shorthand': 'warn',
+    'quote-props': ['warn', 'as-needed'],
+    // TypeScript rules
+    '@typescript-eslint/array-type': [
+      'warn',
+      {
+        default: 'array',
+      },
+    ],
+    '@typescript-eslint/consistent-type-assertions': [
+      'warn',
+      {
+        assertionStyle: 'as',
+        objectLiteralTypeAssertions: 'never',
+      },
+    ],
+    // React rules
+    'react/jsx-fragments': ['warn', 'syntax'], // Shorthand syntax for React fragments
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['ts', 'tsx'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
